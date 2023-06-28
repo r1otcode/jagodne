b3974a71ca63d9ddb7615bf128bef7af761084a4
j4rocode <97634207+j4rocode@users.noreply.github.com>
2023-06-28 11:10:38 +0200

    add next post / prev post navigation
    add

--- components/layouts/singlePost/PostsNavigation/index.tsx
+++ components/layouts/singlePost/PostsNavigation/index.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 import TextSpacer from "../../TextSpacer";
+import Paragraph from "@/components/Typography/Paragraph";
 
 
 export default function PostsNavigation() {
@@ -10,12 +11,42 @@ export default function PostsNavigation() {
     
 
     );
+     const NavWrapperClasses = classNames(
+        'flex', 'justify-between', 'text-light',
+        'xs:py-5 ',
+        'sm:py-5 ',
+        'md:py-[30px]',
+        'lg:py-[50px]',
+        'xl:py-[30px]',
+        '2xl:py-[30px]',
+        '3xl:py-[40px]',
+        '4xl:py-[60px]',
+     )
+     const imgClasses = classNames(
+       'w-[60px]'
+     )
+     const mobileDisplay = classNames(
+        'inline',
+        'xs:hidden',
+        'sm:hidden',
+        'md:hidden',
+        'lg:hidden',
+        'xl:inline',
+        '2xl:inline',
+        '3xl:inline',
+        '4xl:inline',
+     
+     
+     )
   return (
     <>
       <div className={PostsNavigationClasses}>
-       <TextSpacer>
-        
-       </TextSpacer>
+       
+        <div className={NavWrapperClasses}>
+            <div className="flex items-center gap-x-5"><img className={imgClasses} src="./assets/arrow_outline_left_circle.svg" /><Paragraph><span className="xs:hidden">Następny </span><span className={mobileDisplay}>wpis</span></Paragraph></div>
+            <div className="flex items-center gap-x-5"> <Paragraph><span className="xs:hidden">Poprzedni</span> <span className={mobileDisplay}>wpis</span></Paragraph><img className={imgClasses} src="./assets/arrow_outline_right_circle.svg" /></div>
+        </div>
+      
        
       </div>
     </>
